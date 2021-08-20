2d45d90f13069a5f63e62c91f8c6f9f0b3a2adec
Waqas Tahir <wakaztahir@gmail.com>
2021-08-20 13:52:22 +0500

    removed additional helpers

--- Blockly/src/main/java/com/wakaztahir/blockly/utils/ColorHelpers.kt
+++ Blockly/src/main/java/com/wakaztahir/blockly/utils/ColorHelpers.kt
@@ -7,11 +7,11 @@ import androidx.core.graphics.blue
 import androidx.core.graphics.green
 import androidx.core.graphics.red
 
-fun androidx.compose.ui.graphics.Color.toViewColor(): Int {
+internal fun androidx.compose.ui.graphics.Color.toViewColor(): Int {
     return getViewColor(this)
 }
 
-fun getViewColor(color: androidx.compose.ui.graphics.Color): Int {
+internal fun getViewColor(color: androidx.compose.ui.graphics.Color): Int {
     val argbColor = color.toArgb()
     with(argbColor) {
         return Color.argb(alpha, red, green, blue)
